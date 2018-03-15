aa2a455331df5fcfe3a3745e3462c45c5443697c
Maria <mariajescobar10@gmail.com>
2018-03-15 18:21:37 -0500

    initial commit

---
+++ lib/sushi_face.rb
@@ -0,0 +1,9 @@
+require "sushi_face/version"
+
+module SushiFace
+  class Fattener
+    def fatten_up
+      'say eyelidddddddddddddddds'
+    end 
+  end 
+end
---
+++ lib/sushi_face/version.rb
@@ -0,0 +1,3 @@
+module SushiFace
+  VERSION = "0.1.0"
+end
